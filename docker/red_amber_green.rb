857e4fa4be5a6413ffedc155da6f6b02804f8228
Jon Jagger <jon@jaggersoft.com>
2022-05-22 18:07:18 +0100

    Update pattern matching for case when two tests are accidentally given the same name

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,6 +1,7 @@
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
+  return :red   if /^Tests: passed 0, failed 0, ignored 0/.match(output)
   return :red   if /^Tests: passed (\d+), failed [1-9][0-9]*, ignored (\d+)/.match(output)
   return :green if /^Tests: passed (\d+), failed 0, ignored (\d+)/.match(output)
   return :amber
